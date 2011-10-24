93584fc7bd133527a1a99387ea842029dba17158
wilblack21@gmail.com <wblack@Redwood-Linux.(none)>
2011-10-23 23:29:24 -0700

    HAve s table version of flat files.

--- main.py
+++ main.py
@@ -23,38 +23,59 @@ sys.path.append("/mnt/sdcard/sl4a/scripts/")
 import android
 import cherrypy
 
-from templates import home 
+import datetime as dt
+
+from templates import home, header, nav 
+from templates import dirList, sysStat
  
-#cherrypy.config.update("/mnt/sdcard/sl4a/scripts/AndroidServer/site_config.txt") 
+cherrypy.config.update("/mnt/sdcard/sl4a/scripts/AndroidServer/site_config.txt") 
 droid = android.Android()
  
 class Root(object):
+    
+    webcam_dir = "/mnt/sdcard/DCIM/Camera/Webcam/"
+    
     def __init__(self):
         self.droid = android.Android()
  
     @cherrypy.expose
     def index(self):
-        self.droid.vibrate()
         return home
 
     @cherrypy.expose
     def take_pic(self):
-        rs = self.droid.cameraCapturePicture("/mnt/sdcard/DCIM/Camera/Webcam/")     
+        now = dt.datetime.now()
+        fname = dt.datetime.strftime(now,"%Y-%m-%d_%h-%i-%s.jpg")
+        rs = self.droid.cameraCapturePicture("/mnt/sdcard/DCIM/Camera/Webcam/"+fname)     
         return str(rs)     
-
+    
+    @cherrypy.expose
+    def webcam_list(self):
+        rs = os.listdir(self.webcam_dir)
+        rs = dirList(rs)
+        return header + nav + rs
+            
+    
     @cherrypy.expose
     def location(self):
         location = self.droid.getLastKnownLocation().result
         location = location.get('network', location.get('gps'))
         return "LAT: %s, LON: %s" % (location['latitude'],
                                      location['longitude'])
+        
+    @cherrypy.expose
+    def sys_stat(self):    
+        rs = sysStat(self.droid)   
+        return header + nav +rs       
+        
  
 def run():
     cherrypy.config.update({'server.socket_host': '0.0.0.0'})
+    #TODO Add IP address message.
     droid.makeToast("Running quickstart")
     cherrypy.quickstart(Root(), '/')
  
 if __name__ == '__main__':
-    
-    droid.makeToast("Starting webserver")
+    droid.ttsSpeak("Starting webserver.")
+    droid.makeToast("Starting AndroidServer")
     run()
--- templates.py
+++ templates.py
@@ -5,13 +5,51 @@ Created on Oct 23, 2011
 '''
 
 
-home = """
-<h1>Welcome to Wil's Android.</h1>
+header = "<h1>Welcome to Wil's Android.</h1>"
+nav = """
+<ul>
+    <li><a href='/'>Home</a></li>
+    <li><a href='/location/'>Where am I?</a></li>
+    <li><a href='/take_pic/'>Take a Pic</a></li>
+    <li><a href='/webcam_list/'>WebCam Folder</a></li>
+    <li><a href='/sys_stat/'>System Status</a></li>
+</ul>
+"""
+
+
+home = header + nav + """
 <p>You can choose from the following actions.</p>
-    <ul>
-        <li><a href='/location/'>Where am I?</a></li>
-        <li><a href='/take_pic/'>Take a Pic</a></li>
-    </ul>
-    
+       
 <img src="images/cypress.jpg"/>    
 """
+
+def sysStat(droid):
+    html = "<dl>"
+        
+    out = droid.checkWifiState()
+    html += "<dt>checkWifiState</dt>"
+    html += "<dd>%s</dd>" %str(out)    
+    
+    out = droid.wifiGetConnectionInfo()
+    html += "<dt>wifiGetConnectionInfo</dt>"
+    html += "<dd>%s</dd>" %str(out)
+    
+    out = droid.getLastKnownLocation()
+    html += "<dt>getLastKnownLocation</dt>"
+    html += "<dd>%s</dd>" %str(out)
+        
+    html += "</dl>"
+    return html
+
+
+def dirList(list):
+    """
+    Given a list it returns it as an html list on the assumption is
+    a directory listing
+    
+    """
+    html ="<ul>"
+    for l in list:
+        html += """<li>%s</li>""" %(l)
+    html += "</ul>"
+    return html
