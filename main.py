1d7bcc80f316e4c17c8c1f6efd92951a43e078e1
Wil Black <wblack@Redwood-Linux.(none)>
2011-10-23 17:33:49 -0700

    Inital Commit

---
+++ main.py
@@ -0,0 +1,60 @@
+'''
+Created on Oct 23, 2011
+
+@author: wblack
+'''
+
+# -*- coding: utf-8 -*-
+import logging
+# The multiprocessing package isn't
+# part of the ASE installation so
+# we must disable multiprocessing logging
+logging.logMultiprocessing = 0
+
+# Update path to point at cherrypy
+import sys
+import os
+
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+print current_dir
+sys.path.append("/mnt/sdcard/sl4a/scripts/")
+ 
+import android
+import cherrypy
+
+from templates.home import html as home 
+ 
+cherrypy.config.update("/mnt/sdcard/sl4a/scripts/AndroidServer/site_config.txt") 
+droid = android.Android()
+ 
+class Root(object):
+    def __init__(self):
+        self.droid = android.Android()
+ 
+    @cherrypy.expose
+    def index(self):
+        self.droid.vibrate()
+        return home
+
+    @cherrypy.expose
+    def take_pic(self):
+        rs = self.droid.cameraCapturePicture("/mnt/sdcard/DCMI/Webcam/")     
+        return str(rs)     
+
+    @cherrypy.expose
+    def location(self):
+        location = self.droid.getLastKnownLocation().result
+        location = location.get('network', location.get('gps'))
+        return "LAT: %s, LON: %s" % (location['latitude'],
+                                     location['longitude'])
+ 
+def run():
+    cherrypy.config.update({'server.socket_host': '0.0.0.0'})
+    droid.makeToast("Running quickstart")
+    cherrypy.quickstart(Root(), '/')
+ 
+if __name__ == '__main__':
+    
+    droid.makeToast("Starting webserver")
+    run()
---
+++ templates/home.py
@@ -0,0 +1,17 @@
+'''
+Created on Oct 23, 2011
+
+@author: wblack
+'''
+
+
+html = """
+<h1>Welcome to Wil's Android.</h1>
+<p>You can choose from the following actions.</p>
+    <ul>
+        <li><a href='/location/'>Where am I?</a></li>
+        <li><a href='/take_pic/'>Take a Pic</a></li>
+    </ul>
+    
+<img src="images/cypress.jpg"/>    
+"""
